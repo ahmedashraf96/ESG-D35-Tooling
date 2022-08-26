26e95ffd45bf1690574062afaad91ec6422ebe98
Ahmed Ashraf <ahmedashrafelnaqeeb@gmail.com>
2022-08-26 16:26:53 +0200

    Initializing repo and adding example files

---
+++ Example.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#include "Example.h"
+
+int main(void)
+{
+	print_my_name();
+	return 0;
+}
---
+++ Func.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "Example.h"
+
+void print_my_name(void)
+{
+	printf("My name is Ahmed\n");
+	return;
+}	
+
